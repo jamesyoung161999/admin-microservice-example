a5799f452e6fc0372ab7d02de40ea879e1fa6d28
jamesyoung161999 <jamesyoung161999@gmail.com>
2021-03-11 19:20:10 +0000

    add service, and test outline

---
+++ admin/src/services/report.service.js
@@ -0,0 +1,8 @@
+module.exports = {
+    generate: () => {
+
+
+
+        return Promise.resolve({})
+    },
+};
---
+++ admin/src/services/report.service.test.js
@@ -0,0 +1,11 @@
+const {generate} = require('./report.service')
+
+describe("report service", () => {
+    describe("generate report", () => {
+        test("should get user investments", async () => {})
+        test("should get user holdings", async () => {})
+        test("should correctly calculate user holdings", async () => {})
+        test("should return report as csv", async () => {})
+        test("each report line should contain correct details", async () => {})
+    })
+})
